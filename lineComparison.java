7a33575411148db7afcef16eebc6b7ffed5cc7c3
Durga Vaishnavi <durgavaishnavisb@gmail.com>
2021-12-10 13:04:16 -0800

    ADDED the calculation for distance between lines

--- lineComparison.java
+++ lineComparison.java
@@ -2,27 +2,28 @@ import java.util.*;
 public class lineComparison{
         public static void main(String[] args){
                 System.out.println("Welcome to Line Comparison Problem");
-                float x1;
+		double x1;
                 System.out.println("Enter value for x1: ");
                 Scanner s1 = new Scanner(System.in);
                 x1=s1.nextFloat();
-                float y1;
+                double y1;
                 System.out.println("Enter value for y1: " );
                 Scanner s2 = new Scanner(System.in);
                 y1=s2.nextFloat();
-                float x2;
+                double x2;
                 System.out.println("Enter value for x2: " );
                 Scanner s3 = new Scanner(System.in);
                 x2=s3.nextFloat();
-                float y2;
+                double y2;
                 System.out.println("Enter value for y2: " );
                 Scanner s4 = new Scanner(System.in);
                 y2=s4.nextFloat();
-                float d1 = (x2-x1);
-                float d2 = (y2-y1);
-                float square1 = d1*d1;
-                float square2 = d2*d2;
+                double d1 = (x2-x1);
+                double d2 = (y2-y1);
+                double square1 = Math.pow(d1,2);
+                double square2 = Math.pow(d2,2);
                 System.out.println("The Distanve between two lines :" + Math.sqrt(square1+square2));
 
-}
+
+                }
 }
